25adf8d011a5f0246784187a72083eb2c64f4c5a
liugang01 <liugang01@jiuqi.com.cn>
2019-12-20 15:41:43 +0800

    核心代码修整 Signed-off-by: liugang01 <liugang01@jiuqi.com.cn>

--- MyDistributedLock/src/main/java/com/kenny/demo/distrilock/controller/MyController.java
+++ MyDistributedLock/src/main/java/com/kenny/demo/distrilock/controller/MyController.java
@@ -45,22 +45,26 @@ public class MyController {
 		return checkStockResult;
 	}
 	
+
 	/**
-	 *  检验库存
-	 * @return
-	 */
+	  * 校验库存
+     * @param good  商品信息
+     * @return 接口结果信息封装类
+     */
 	private MyResultCode checkStock(Order good) {
 		if(good == null || StringUtils.isEmpty(good.getGoodName())) {
+		    //商品信息为空直接返回
 			return MyResultCode.PARAM_IS_BLANK;
 		}
-		
+		//自定义的key
 		String goodLockKey = good.getGoodName();
 		//公平锁
 		RLock fairLock = redisson.getFairLock(goodLockKey);
 		try {
-		    //尝试加锁，最多阻塞10秒
-			boolean res = fairLock.tryLock(10,TimeUnit.SECONDS);
+		    //尝试加锁，最多阻塞10秒，上锁以后20秒自动解锁
+			boolean res = fairLock.tryLock(10,20,TimeUnit.SECONDS);
 			if(!res) {
+			    //加锁失败
 				return MyResultCode.PROCESS_FAULT;
 			}
 			//预减库存 这里只是模拟一下，实际库存应该是从数据库或者本地缓存中读取
@@ -68,7 +72,7 @@ public class MyController {
 			if(newStock < 1) {
 				return MyResultCode.PROCESS_EMPTY;
 			}
-			//更新库存，这里只是模拟一下，实际写库存应该是写入数据库并且更新本地缓存
+			//更新库存，这里只是模拟一下，实际写库存应该考虑写入数据库并且更新本地缓存
 			STOCK_QUANTITY = newStock;
 			log.info("减1个库存，库存变为[{}]",STOCK_QUANTITY);
 			//释放锁
